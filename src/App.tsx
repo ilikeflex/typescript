529a022ea0fae8f2bcf9d712fc390927c58434d9
ilikeflex <ilikeflex@yahoo.com>
2020-11-05 01:38:40 -0500

    fix for build

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,3 @@
+import React from "react";
 const App = () => <div>Hello React App from scratch</div>;
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
-/// <reference path="App.tsx" />
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App"
 
 ReactDOM.render(<App />, document.getElementById("root"));
